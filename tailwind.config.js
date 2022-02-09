142c1c979930ba58b4a5fd9933b0c83ba5c957b4
duty-bound <duty_bound@ail.com>
2022-02-10 00:16:02 +0100

    Implements basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   content: [],
   theme: {
+    colors: {
+      primary: {
+        200: 'rgb(59, 89, 152)'
+      },
+      secondary: {
+        50: 'rgb(255, 255, 255)',
+        100: 'rgb(200, 200, 200)',
+        200: 'rgb(75, 75, 75)',
+        900: 'rgb(0, 0, 0)'
+      }
+    },
     extend: {}
   },
   plugins: []
