79206bc121a7b80f521b824d83b2988cacd5d092
duty-bound <duty_bound@ail.com>
2022-02-09 22:26:55 +0100

    Initializes Redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,16 @@
 import { configureStore, EnhancedStore } from '@reduxjs/toolkit'
-import { ui, uiInitialState, IUI } from './ui-slice'
+import { movies, moviesInitialState, IMovies } from './movies-slice'
 
 export interface IState {
-  ui: IUI
+  movies: IMovies
 }
 
 const initialState: IState = {
-  ui: uiInitialState
+  movies: moviesInitialState
 }
 
 const reducer = {
-  ui
+  movies
 }
 
 const initStore = (preloadedState = initialState): EnhancedStore => configureStore({
