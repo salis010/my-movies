2282ad79239be4fce3047e2af25cbf1a90238f26
duty-bound <duty_bound@ail.com>
2022-02-10 19:26:36 +0100

    Creats input component for Search feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { GenresDropdown } from './components/GenresDropdown'
+import { Search } from './components/Search'
 import { MoviesGrid } from './components/MoviesGrid'
 import { fetchGenres } from './store/movies/thunks/fetchGenres'
 
@@ -8,13 +9,16 @@ export const App: FunctionComponent = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(fetchGenres())
+    // dispatch(fetchGenres())
   })
 
   return (
     <div className='flex flex-col mx-4 sm:mx-8'>
       <h1 className='m-16 font-bold text-7xl text-primary-200'>My Movies</h1>
-      <GenresDropdown />
+      <div className='flex justify-between w-full'>
+        <GenresDropdown />
+        <Search />
+      </div>
       <MoviesGrid />
     </div>
   )
